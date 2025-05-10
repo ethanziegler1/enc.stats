9f26758fe2c1a6c67b87e6a3922033346390418c
Ethan <ethan.ziegler1@gmail.com>
2025-05-09 22:54:34 -0400

    progress on time frame script

--- static/js/song-time-frame.js
+++ static/js/song-time-frame.js
@@ -1,10 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
-    let songList = document.querySelector("#song-list");
-    let top_songs = songList.dataset.top_songs;
+    let songListContainer = document.querySelector("#song-list-container");
+    console.log(songListContainer);
 
-    for (let i = 0; i < top_songs; i++) {
+    let topSongs = songListContainer.dataset.topSongs;
+    console.log(topSongs);
+
+    topSongs.forEach((song, index) => {
         let li = document.createElement("li");
-        li.innerText = `Song ${i}`;
-        songList.appendChild(li);
-    }
+        li.innerHTML = `
+            <span>${index + 1}.</span>
+            <img src="${song.album.images[0].url}" alt="${song.name}" width="50" height="50" />
+            <p>${song.name} - ${song.artists[0]?.name || 'Unknown Artist'}</p>
+        `;
+        songListContainer.appendChild(li);
+    });
 });
